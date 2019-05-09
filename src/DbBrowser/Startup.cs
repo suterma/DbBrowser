3737cf8d484d5dfef9ecb0337f0c739ffb519c71
Marcel <github-repositories@marcelsuter.ch>
2019-05-09 23:07:32 +0200

    Update Startup.cs
    Added logger

--- src/DbBrowser/Startup.cs
+++ src/DbBrowser/Startup.cs
@@ -9,15 +9,25 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace DbBrowser
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private readonly ILogger _logger;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Startup"/> class.
+        /// </summary>
+        /// <param name="configuration">The configuration.</param>
+        /// <param name="logger">The logger to use.</param>
+        public Startup(IConfiguration configuration, ILogger<Startup> logger)
         {
             Configuration = configuration;
+            _logger = logger;
         }
+
 
         public IConfiguration Configuration { get; }
 
@@ -57,11 +67,20 @@ namespace DbBrowser
                 app.UseHsts();
             }
 
+            //The logger added as simple form of middleware
+            app.Use(async (context, next) =>
+            {
+                // Do work that doesn't write to the Response.
+                _logger.LogInformation("Handling Request Path:" + context.Request.Path);
+                await next.Invoke();
+                // Do logging or other work that doesn't write to the Response.
+                _logger.LogInformation("Handeled Request Path:" + context.Request.Path);
+            });
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
             app.UseSession();
-
 
             app.UseMvc(routes =>
             {
